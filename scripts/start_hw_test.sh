065287b0383ee35d11a095c847df03e384aabe9c
Sturla Lange <sturla22@gmail.com>
2020-06-08 10:59:24 +0200

    Move scripts to their own dir

---
+++ scripts/start_hw_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd $parent_path/..
+
+./build.sh -w 
+
+avrdude -p atmega328p -c arduino -P /dev/ttyACM0 -b115200 -D -Uflash:w:"build/ayab_HW_TEST_uno.hex":i
+
+miniterm -e --raw /dev/ttyACM0 115200
